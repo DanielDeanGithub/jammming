0a8466430d942ce64c14ecabd3e0fc59c8bdf545
DanielDeanGithub <github-new@inboxrefresh.com>
2024-10-14 07:51:01 +0100

    Add onChange event handler to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,7 +4,7 @@ function SearchBar() {
     const [search, setSearch] = useState("");
 
     return (
-        <input id="Search" />
+        <input id="Search" value={search} onChange={e => setSearch(e.target.value)}/>
     );
 };
 
