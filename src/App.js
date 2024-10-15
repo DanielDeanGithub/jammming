7f8f09c7856c9d1904aa5b3b66c4f474d819d004
DanielDeanGithub <github-new@inboxrefresh.com>
2024-10-15 08:15:33 +0100

    Import SubmitButton

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar/SearchBar.js';
 import SearchResults from './components/SearchResults/SearchResults.js';
+import SearchButton from './components/SubmitButton/SubmitButton.js';
 
 function App() {
   const [search, setSearch] = useState("");
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <SearchBar value={search}  onChange={e => setSearch(e.target.value)}/>
+        <SearchButton />
         <SearchResults results={[search]}/>
       </header>
     </div>
