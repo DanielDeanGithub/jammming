08a3fc714669086f9c5de7a5548ae920f99deb11
DanielDeanGithub <github-new@inboxrefresh.com>
2024-10-17 21:39:29 +0100

    Update to include working search test

--- src/App.js
+++ src/App.js
@@ -3,28 +3,58 @@ import './App.css';
 import SearchBar from './components/SearchBar/SearchBar.js';
 import SearchResults from './components/SearchResults/SearchResults.js';
 import SearchButton from './components/SubmitButton/SubmitButton.js';
-import Spotify from './utilities/Spotify.js';
+import { initialiseSpotify, testRefresh, searchTest } from './utilities/Spotify.js';
 
 function App() {
   const [search, setSearch] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [login, setLogin] = useState(false);
   
   useEffect(() => {
-    if (search === '') document.getElementById("searchBar").value = search;
+    //if (search === '') document.getElementById("searchBar").value = search;
   }, [search])
 
   const searchButtonClickHandler = () => {
     setSearchResults([search]);
     setSearch('');
-    Spotify();
+    searchTest();
   };
+
+  const authoriseApp = () => {
+    initialiseSpotify() ? setLogin(true) : setLogin(false)
+  }
+
+  if (!login) {
+    return (
+      <button onClick={authoriseApp}>Allow App</button>
+    )
+  }
   
   return (
     <div className='App'>
       <header className='App-header'>
+        <button onClick={testRefresh}>TEST</button>
+
         <SearchBar value={search} onChange={e => setSearch(e.target.value)}/>
         <SearchButton onClick={searchButtonClickHandler}/>
         <SearchResults results={searchResults}/>
+
+
+        <h1>Display your Spotify profile data</h1>
+        <section id="profile">
+        <h2>Logged in as <span id="displayName"></span></h2>
+        <span id="avatar"></span>
+        <ul>
+            <li>User ID: <span id="id"></span></li>
+            <li>Email: <span id="email"></span></li>
+            <li>Spotify URI: <a id="uri" href="#"></a></li>
+            <li>Link: <a id="url" href="#"></a></li>
+            <li>Profile Image: <span id="imgUrl"></span></li>
+        </ul>
+        </section>
+
+
+
       </header>
     </div>
   );
--- src/utilities/Spotify.js
+++ src/utilities/Spotify.js
@@ -3,7 +3,10 @@ const { API_KEY, API_URL } = config;
 const clientId = API_KEY;
 const redirectUri = API_URL;
 
-export const redirectToAuthCodeFlow = async (clientId) => {
+const args = new URLSearchParams(window.location.search);
+const code = args.get('code');
+
+const redirectToAuthCodeFlow = async (clientId) => {
     const verifier = generateCodeVerifier(128);
     const challenge = await generateCodeChallenge(verifier);
 
@@ -39,9 +42,8 @@ const generateCodeChallenge = async (codeVerifier) => {
         .replace(/=+$/, '');
 }
 
-export const getAccessToken = async (clientId, code) => {
+const getAccessToken = async (clientId, code) => {
     const verifier = localStorage.getItem("verifier");
-
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
@@ -55,7 +57,11 @@ export const getAccessToken = async (clientId, code) => {
         body: params
     });
 
-    const { access_token } = await result.json();
+    const {access_token, refresh_token} = await result.json();
+    
+    localStorage.setItem('access_token', access_token);
+    localStorage.setItem('refresh_token', refresh_token);
+
     return access_token;
 }
 
@@ -67,9 +73,51 @@ async function fetchProfile(token) {
     return await result.json();
 }
 
-const Spotify = async () => {
-    const code = undefined;
+const populateUI = (profile) => {
+    console.log(profile);
 
+    document.getElementById("displayName").innerText = profile.display_name;
+    if (profile.images[0]) {
+        const profileImage = new Image(200, 200);
+        profileImage.src = profile.images[0].url;
+        document.getElementById("avatar").appendChild(profileImage);
+        document.getElementById("imgUrl").innerText = profile.images[0].url;
+    }
+    document.getElementById("id").innerText = profile.id;
+    document.getElementById("email").innerText = profile.email;
+    document.getElementById("uri").innerText = profile.uri;
+    document.getElementById("uri").setAttribute("href", profile.external_urls.spotify);
+    document.getElementById("url").innerText = profile.href;
+    document.getElementById("url").setAttribute("href", profile.href);
+}
+
+const getRefreshToken = async () => {
+    // refresh token that has been previously stored
+    const refreshToken = localStorage.getItem('refresh_token');
+    const url = "https://accounts.spotify.com/api/token";
+ 
+     const payload = {
+       method: 'POST',
+       headers: {
+         'Content-Type': 'application/x-www-form-urlencoded'
+       },
+       body: new URLSearchParams({
+         grant_type: 'refresh_token',
+         refresh_token: refreshToken,
+         client_id: clientId
+       }),
+     }
+     const body = await fetch(url, payload);
+     const response = await body.json();
+     console.log(response);
+ 
+     localStorage.setItem('access_token', response.accessToken);
+     if (response.refreshToken) {
+       localStorage.setItem('refresh_token', response.refreshToken);
+     }
+}
+
+export const initialiseSpotify = async () => {
     if (!code) {
         redirectToAuthCodeFlow(clientId);
     } else {
@@ -77,11 +125,24 @@ const Spotify = async () => {
         const profile = await fetchProfile(accessToken);
         populateUI(profile);
     }
-    
-    function populateUI(profile) {
-        // TODO: Update UI with profile data
-    }
-
 }
 
-export default Spotify;
+export const testRefresh = async () => {
+    console.log(`Current token: ${localStorage.getItem('refresh_token')}`);
+    await getRefreshToken();
+    console.log(`New token: ${localStorage.getItem('refresh_token')}`);
+}
+
+export const searchTest = async () => {
+    const token = localStorage.getItem('access_token');
+    const url = "https://api.spotify.com/v1/search?q=Carly+Rae+Jepsen&type=artist&market=GB&limit=1";
+    
+    const result = await fetch(url, {
+        method: "GET", headers: { Authorization: `Bearer ${token}` }
+    });
+
+    const details = await result.json();
+
+    console.log(details);
+    return details;
+}
