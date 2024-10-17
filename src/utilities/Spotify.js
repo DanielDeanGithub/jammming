57f67761d8ebeadbae57e1c3cd5a155edfb906ae
DanielDeanGithub <github-new@inboxrefresh.com>
2024-10-17 08:24:31 +0100

    Update scope of Spotify.js

--- src/utilities/Spotify.js
+++ src/utilities/Spotify.js
@@ -2,9 +2,46 @@ import config from './config.js';
 
 const { API_KEY, API_URL } = config;
 
+const clientId = API_KEY;
+const redirectUri = API_URL;
+
+const redirectToAuthCodeFlow = async (clientId) => {
+    const verifier = generateCodeVerifier(128);
+    const challenge = await generateCodeChallenge(verifier);
+
+    localStorage.setItem("verifier", verifier);
+
+    const params = new URLSearchParams();
+    params.append("client_id", clientId);
+    params.append("response_type", "code");
+    params.append("redirect_uri", redirectUri);
+    params.append("scope", "user-read-private user-read-email");
+    params.append("code_challenge_method", "S256");
+    params.append("code_challenge", challenge);
+
+    document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+const generateCodeVerifier = (length) => {
+    let text = '';
+    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+    for (let i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+const generateCodeChallenge = async (codeVerifier) => {
+    const data = new TextEncoder().encode(codeVerifier);
+    const digest = await window.crypto.subtle.digest('SHA-256', data);
+    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+}
+
 const Spotify = async () => {
-    const clientId = API_KEY;
-    const redirectUri = API_URL;
     const code = undefined;
 
     if (!code) {
@@ -15,41 +52,9 @@ const Spotify = async () => {
         populateUI(profile);
     }
 
-    async function redirectToAuthCodeFlow(clientId) {
-        const verifier = generateCodeVerifier(128);
-        const challenge = await generateCodeChallenge(verifier);
-
-        localStorage.setItem("verifier", verifier);
-
-        const params = new URLSearchParams();
-        params.append("client_id", clientId);
-        params.append("response_type", "code");
-        params.append("redirect_uri", redirectUri);
-        params.append("scope", "user-read-private user-read-email");
-        params.append("code_challenge_method", "S256");
-        params.append("code_challenge", challenge);
-
-        document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
-    }
-
-    function generateCodeVerifier(length) {
-        let text = '';
-        let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     
-        for (let i = 0; i < length; i++) {
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-    }
 
-    async function generateCodeChallenge(codeVerifier) {
-        const data = new TextEncoder().encode(codeVerifier);
-        const digest = await window.crypto.subtle.digest('SHA-256', data);
-        return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-            .replace(/\+/g, '-')
-            .replace(/\//g, '_')
-            .replace(/=+$/, '');
-    }
+
 
     async function getAccessToken(clientId, code) {
     // TODO: Get access token for code
