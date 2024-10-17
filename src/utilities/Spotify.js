c5c9a65b345d8fc4c3863ead4ac00737a1216903
DanielDeanGithub <github-new@inboxrefresh.com>
2024-10-17 07:45:30 +0100

    Add outline of Spotify app

--- src/utilities/Spotify.js
+++ src/utilities/Spotify.js
@@ -2,9 +2,35 @@ import config from './config.js';
 
 const { API_KEY, API_URL } = config;
 
-const Spotify = {
-    clientId: API_KEY,
-    redirectUri: API_URL
+const Spotify = async () => {
+    const clientId = API_KEY;
+    const redirectUri = API_URL;
+    const code = undefined;
+
+    if (!code) {
+        redirectToAuthCodeFlow(clientId);
+    } else {
+        const accessToken = await getAccessToken(clientId, code);
+        const profile = await fetchProfile(accessToken);
+        populateUI(profile);
+    }
+
+    async function redirectToAuthCodeFlow(clientId) {
+        // TODO: Redirect to Spotify authorization page
+    }
+
+    async function getAccessToken(clientId, code) {
+    // TODO: Get access token for code
+    }
+
+    async function fetchProfile(token) {
+        // TODO: Call Web API
+    }
+
+    function populateUI(profile) {
+        // TODO: Update UI with profile data
+    }
+
 }
 
 export default Spotify;
