4e64539d4514c7452371655f57e877acf46cc948
DanielDeanGithub <github-new@inboxrefresh.com>
2024-10-19 23:33:33 +0100

    Add SearchResults.css

--- src/components/SearchResults/SearchResults.js
+++ src/components/SearchResults/SearchResults.js
@@ -1,17 +1,30 @@
 import React from 'react'
+import './SearchResults.css'
 
 const SearchResults = ({results}) => {
   return (
-    <div id='searchResults'>
+    <div id='search-results'>
       <h2>Search Results</h2>
       {
         results.map((e) => {
           return (
             <div key={e['trackId']} className='result'>
-              <h3>{e['trackName']}</h3>
-              <h4>{e['artists']} - {e['albumName']}</h4> 
-              <img src={e['albumArtwork']} alt={e['trackName'] + ' Artwork'} />
-              <audio src={e['preview']} controls />
+              <div className='artwork-container'>
+                <img className='artwork' src={e['albumArtwork']} alt={e['trackName'] + ' Artwork'} />
+              </div>
+              <div className='text'>
+                <h3>{e['trackName']}</h3>
+
+                <div className='sub-text'>
+                  <h4>{e['artists']}</h4> 
+                  <h4>-</h4>
+                  <h4>{e['albumName']}</h4>
+                </div>
+              </div>
+              <audio id={e['trackId']} src={e['preview']} />
+              <button onClick={() => document.getElementById(e['trackId']).paused 
+                  ? document.getElementById(e['trackId']).play()
+                  : document.getElementById(e['trackId']).pause()}>Play/Pause</button>
             </div>
           )
         })
